516d8cb7f9cbe360da80a40f0efc72f07ea281d2
Dave Morz <davidscout2010@hotmail.com>
2018-03-22 22:52:08 -0600

    Poniendo una separación

--- Ejercicio2-php.php
+++ Ejercicio2-php.php
@@ -2,12 +2,13 @@
 <html>
   <head>
     <meta charset="utf-8">
-    <title>Calcular Peso Fisico apartir de una fuerza</title>
+    <title>Calcular Peso Fisico Apartir de Una Fuerza</title>
   </head>
   <body>
     <br>
         <label for="peso">Introduzca la fuerza (NEWTONS): </label>
         <input type="text" placeholder="Escriba la fuerza" name="fuerza" class="form-control" id="fuerza">
+    <br>
         <?php
         $fuerza = $_POST['fuerza'];
         $gravedad = '9.8';
